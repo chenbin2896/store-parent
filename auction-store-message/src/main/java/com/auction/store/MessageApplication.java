7b8144a2026fb3c025d1202707f4017230ac9227
EDZ <3375793023@qq.com>
2019-10-09 16:22:25 +0800

    客服中心

---
+++ auction-store-message/src/main/java/com/auction/store/MessageApplication.java
@@ -0,0 +1,12 @@
+package com.auction.store;
+
+import com.auction.store.server.Message;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MessageApplication.class);
+    }
+}
---
+++ auction-store-message/src/main/java/com/auction/store/server/Message.java
@@ -0,0 +1,32 @@
+package com.auction.store.server;
+
+import com.alibaba.fastjson.JSON;
+import lombok.Data;
+
+
+@Data
+public class Message {
+    public static final String ENTER = "ENTER";
+    public static final String SPEAK = "SPEAK";
+    public static final String QUIT = "QUIT";
+
+    private String type;//消息类型
+
+    private String username; //发送人
+
+    private String msg; //发送消息
+
+    private int onlineCount; //在线用户数
+
+    public static String jsonStr(String type, String username, String msg, int onlineTotal) {
+        return JSON.toJSONString(new Message(type, username, msg, onlineTotal));
+    }
+
+    public Message(String type, String username, String msg, int onlineCount) {
+        this.type = type;
+        this.username = username;
+        this.msg = msg;
+        this.onlineCount = onlineCount;
+    }
+
+}
