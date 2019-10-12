9c8dc8e1d7ff4b2ba2a3aade881030059b81bb99
EDZ <3375793023@qq.com>
2019-10-12 17:21:15 +0800

    style(auction-store-web):
    BREAKING CHANGE: 优化

---
+++ auction-store-idcard/src/main/java/com/auction/store/idcard/controller/UploadController.java
@@ -0,0 +1,13 @@
+package com.auction.store.idcard.controller;
+
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "上传身份证")
+@RestController
+@RequestMapping("/v1/card")
+public class UploadController {
+
+}
